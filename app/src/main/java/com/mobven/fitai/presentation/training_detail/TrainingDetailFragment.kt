08b93f496ecf83bb77cb2081d3dc0bef591e69ee
Furkan <kvkfurkan@hotmail.com>
2024-06-09 14:39:31 +0300

    training detail screen added.

---
+++ app/src/main/java/com/mobven/fitai/presentation/training_detail/TrainingDetailFragment.kt
@@ -0,0 +1,13 @@
+package com.mobven.fitai.presentation.training_detail
+
+
+import com.mobven.fitai.databinding.FragmentTrainingDetailBinding
+import com.mobven.fitai.presentation.base.BaseFragment
+
+class TrainingDetailFragment : BaseFragment<FragmentTrainingDetailBinding>(FragmentTrainingDetailBinding::inflate) {
+
+    override fun observeUi() {
+        super.observeUi()
+    }
+
+}
