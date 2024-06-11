f1e6b7484e9e8683a5173dd9f53a338a319e1282
Furkan <kvkfurkan@hotmail.com>
2024-06-12 01:43:20 +0300

    bottom sheet directions added.

--- app/src/main/java/com/mobven/fitai/presentation/home/create_plan/BottomSheetFragment.kt
+++ app/src/main/java/com/mobven/fitai/presentation/home/create_plan/BottomSheetFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.cardview.widget.CardView
+import androidx.navigation.fragment.findNavController
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.mobven.fitai.R
 
@@ -17,6 +19,16 @@ class BottomSheetFragment : BottomSheetDialogFragment() {
         val view = inflater.inflate(R.layout.fragment_bottom_sheet, container, false)
 
         val closeButton: ImageView = view.findViewById(R.id.closeButton)
+        val addTraining: CardView = view.findViewById(R.id.addTrainingPlanCard)
+        val addNutrition: CardView = view.findViewById(R.id.addNutritionPlanCard)
+
+        addTraining.setOnClickListener {
+            findNavController().navigate(R.id.action_bottomSheetFragment_to_trainingFragment)
+        }
+
+        addNutrition.setOnClickListener {
+            findNavController().navigate(R.id.action_bottomSheetFragment_to_nutritionFragment)
+        }
 
         closeButton.setOnClickListener {
             dismiss()
