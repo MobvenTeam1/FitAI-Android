bec6d452512d329df9981977f8d059ad5eb870af
Furkan <kvkfurkan@hotmail.com>
2024-06-12 17:34:47 +0300

    SignUp terms pop-up added.

--- app/src/main/java/com/mobven/fitai/presentation/login/sign_up/screens/RegisterFragment.kt
+++ app/src/main/java/com/mobven/fitai/presentation/login/sign_up/screens/RegisterFragment.kt
@@ -1,11 +1,12 @@
 package com.mobven.fitai.presentation.login.sign_up.screens
 
+import android.app.AlertDialog
 import android.util.Log
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
 import com.mobven.fitai.R
-import com.mobven.fitai.common.ResponseState
 import com.mobven.fitai.common.SharedPreferencesHelper
 import com.mobven.fitai.data.model.dto.SignUpDto
 import com.mobven.fitai.databinding.FragmentRegisterBinding
@@ -22,10 +23,22 @@ class RegisterFragment : BaseFragment<FragmentRegisterBinding>(FragmentRegisterB
 
     private val signInViewModel: SignInViewModel by viewModels()
     private val signUpViewModel: SignUpViewModel by viewModels()
+    private var checkBoxStatus = false
 
     override fun observeUi() {
         with(binding) {
+
+            signUpTermsCheckbox.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    showPrivacyPolicyDialog()
+                    checkBoxStatus = true
+                }else{
+                    checkBoxStatus = false
+                }
+            }
+
             btnSignUpContinue.setOnClickListener {
+
                 val editTextEmail = editTextEmailSignUp.text.toString()
                 val editTextPassword = editTextPasswordSignUp.text.toString()
                 val editTextPasswordAgain = editTextPasswordAgainSignUp.text.toString()
@@ -43,6 +56,12 @@ class RegisterFragment : BaseFragment<FragmentRegisterBinding>(FragmentRegisterB
                     signInViewModel.isEmpty(editTextUserName)
                 textInputLayoutSurnameSignUp.helperText =
                     signInViewModel.isEmpty(editTextUserName)
+
+                if (!checkBoxStatus){
+                    Toast.makeText(requireContext(), "Lütfen şartları ve gizlilik politikasını kabul edin", Toast.LENGTH_SHORT).show()
+                    return@setOnClickListener
+                }
+
 
                 if (allFieldsValid()) {
                     signUpViewModel.onAction(
@@ -98,11 +117,29 @@ class RegisterFragment : BaseFragment<FragmentRegisterBinding>(FragmentRegisterB
 
     private fun allFieldsValid(): Boolean {
         return with(binding) {
-            textInputLayoutEmailSignUp.helperText == null
+                    textInputLayoutEmailSignUp.helperText == null
                     && textInputLayoutPasswordSignUp.helperText == null
                     && textInputLayoutPasswordAgainSignUp.helperText == null
                     && textInputLayoutNicknameSignUp.helperText == null
         }
+    }
+
+    private fun showPrivacyPolicyDialog() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("Şartlar ve Gizlilik Politkası")
+        builder.setMessage("Bu uygulamanın şartlarını ve gizlilik sözleşmesini okudum ve kabul ediyorum.")
+
+        builder.setPositiveButton("Okudum, Onaylıyorum") { dialog, _ ->
+            dialog.dismiss()
+        }
+
+        builder.setNegativeButton("İptal") { dialog, _ ->
+            binding.signUpTermsCheckbox.isChecked = false
+            dialog.dismiss()
+        }
+
+        val dialog = builder.create()
+        dialog.show()
     }
 }
 
