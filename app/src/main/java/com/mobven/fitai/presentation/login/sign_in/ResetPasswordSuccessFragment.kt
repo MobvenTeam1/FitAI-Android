997a8c31bc2134d22f3ed1146620df7e8c425795
Furkan <kvkfurkan@hotmail.com>
2024-06-13 12:37:11 +0300

    checkbox bug fixed

--- app/src/main/java/com/mobven/fitai/presentation/login/sign_in/ResetPasswordSuccessFragment.kt
+++ app/src/main/java/com/mobven/fitai/presentation/login/sign_in/ResetPasswordSuccessFragment.kt
@@ -4,10 +4,10 @@ import androidx.navigation.NavDirections
 import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
 import com.mobven.fitai.R
-import com.mobven.fitai.databinding.FragmentResetPasswordSuccesBinding
+import com.mobven.fitai.databinding.FragmentResetPasswordSuccessBinding
 import com.mobven.fitai.presentation.base.BaseFragment
 
-class ResetPasswordSuccessFragment : BaseFragment<FragmentResetPasswordSuccesBinding>(FragmentResetPasswordSuccesBinding::inflate){
+class ResetPasswordSuccessFragment : BaseFragment<FragmentResetPasswordSuccessBinding>(FragmentResetPasswordSuccessBinding::inflate){
 
     override fun observeUi() {
         binding.loginButton.setOnClickListener {
